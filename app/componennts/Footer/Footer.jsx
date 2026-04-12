98d91a4e28d9e3a388419435bb9caf16b2d7e141
Aswin Krishna <your_email@example.com>
2026-04-13 00:08:34 +0530

    added work on progressto evry page

--- app/componennts/Footer/Footer.jsx
+++ app/componennts/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Footer.css";
+import Link from "next/link";
+
 function Footer() {
   return (
     <footer className="footer">
@@ -12,21 +14,21 @@ function Footer() {
         <div className="footer-links">
           <div className="footer-column">
             <h4>Product</h4>
-            <a href="#">Features</a>
-            <a href="#">Pricing</a>
-            <a href="#">Documentation</a>
+            <Link href="/Progress">Features</Link>
+            <Link href="/Progress">Pricing</Link>
+            <Link href="/Progress">Documentation</Link>
           </div>
 
           <div className="footer-column">
             <h4>Company</h4>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
+            <Link href="/Progress">About</Link>
+            <Link href="/Progress">Contact</Link>
           </div>
 
           <div className="footer-column">
             <h4>Legal</h4>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Service</a>
+            <Link href="/Progress">Privacy Policy</Link>
+            <Link href="/Progress">Terms of Service</Link>
           </div>
         </div>
       </div>
